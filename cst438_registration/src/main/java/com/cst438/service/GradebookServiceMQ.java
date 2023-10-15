7edf9240ed78e3b01fb7bbc1a891d212a53da63e
AUTUMN <661benton@gmail.com>
2023-10-15 13:56:12 -0700

    assignment4 corrections

--- cst438_registration/src/main/java/com/cst438/service/GradebookServiceMQ.java
+++ cst438_registration/src/main/java/com/cst438/service/GradebookServiceMQ.java
@@ -6,11 +6,11 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.cst438.domain.FinalGradeDTO;
-import com.cst438.domain.Student;
 import com.cst438.domain.Enrollment;
 import com.cst438.domain.EnrollmentDTO;
 import com.cst438.domain.EnrollmentRepository;
@@ -27,18 +27,22 @@ public class GradebookServiceMQ implements GradebookService {
 	EnrollmentRepository enrollmentRepository;
 	
 	Queue gradebookQueue = new Queue("gradebook-queue", true);
+	
+	@Bean
+	Queue createQueue() {
+		return new Queue("registration-queue");
+	}
 
 	// send message to grade book service about new student enrollment in course
 	@Override
 	public void enrollStudent(String student_email, String student_name, int course_id) {
 		System.out.println("Start Message "+ student_email +" " + course_id); 
-		// create EnrollmentDTO, convert to JSON string and send to gradebookQueue
-		// TODO
-		Enrollment e = enrollmentRepository.findByEmailAndCourseId(student_email, course_id);
-		EnrollmentDTO dto = new EnrollmentDTO(e.getEnrollment_id(), student_email, student_name, course_id);
-		String dtoString = asJsonString(dto);
-		rabbitTemplate.convertAndSend(gradebookQueue.getName(), dtoString);
-		
+		// create EnrollmentDTO and send to gradebookQueue
+		EnrollmentDTO dto = new EnrollmentDTO(0, student_email, student_name, course_id);
+		String json = asJsonString(dto);
+		System.out.println(json);
+		rabbitTemplate.convertAndSend(gradebookQueue.getName(), json);
+		System.out.println("End Message");  	
 	}
 	
 	@RabbitListener(queues = "registration-queue")
@@ -49,16 +53,15 @@ public class GradebookServiceMQ implements GradebookService {
 		 * for each student grade in courseDTOG,  find the student enrollment 
 		 * entity and update the grade.
 		 */
-		
-		// deserialize the string message to FinalGradeDTO[] 
 		FinalGradeDTO[] grades = fromJsonString(message, FinalGradeDTO[].class);
-		
-		// TODO
-		for(FinalGradeDTO grade: grades) {
-			Enrollment e = enrollmentRepository.findByEmailAndCourseId(grade.studentEmail(), grade.courseId());
-			e.setCourseGrade(grade.grade());
-			enrollmentRepository.save(e);
-		}		
+		System.out.println("Grades received "+grades.length);
+		for (FinalGradeDTO dto: grades) {
+			Enrollment enrollment = enrollmentRepository.findByEmailAndCourseId(dto.studentEmail(), dto.courseId());
+			if (enrollment != null) {
+				enrollment.setCourseGrade(dto.grade());
+				enrollmentRepository.save(enrollment);
+			}
+		}
 	}
 	
 	private static String asJsonString(final Object obj) {
--- cst438_registration/src/main/java/com/cst438/service/GradebookServiceREST.java
+++ cst438_registration/src/main/java/com/cst438/service/GradebookServiceREST.java
@@ -1,11 +1,9 @@
 package com.cst438.service;
 
-import org.hibernate.mapping.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,13 +11,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.cst438.domain.FinalGradeDTO;
-import com.cst438.domain.Student;
-import com.cst438.domain.StudentRepository;
-import com.cst438.domain.Course;
-import com.cst438.domain.CourseRepository;
 import com.cst438.domain.Enrollment;
 import com.cst438.domain.EnrollmentDTO;
 import com.cst438.domain.EnrollmentRepository;
@@ -34,47 +27,20 @@ public class GradebookServiceREST implements GradebookService {
 	@Value("${gradebook.url}")
 	private static String gradebook_url;
 	
-	@Autowired
-	StudentRepository studentRepository;
-	
-	@Autowired
-	EnrollmentRepository enrollmentRepository;
-	
-	@Autowired
-	CourseRepository courseRepository;
+	public GradebookServiceREST() {
+		System.out.println("gradebook service rest " );
+	}
 
 	@Override
 	public void enrollStudent(String student_email, String student_name, int course_id) {
 		System.out.println("Start Message "+ student_email +" " + course_id); 
-
-		Student student = studentRepository.findByEmail(student_email);
-		Course course  = courseRepository.findById(course_id).orElse(null);
-		Enrollment enroll = enrollmentRepository.findByEmailAndCourseId(student_email, course_id);
-		
-		// TODO use RestTemplate to send message to gradebook service
-		RestTemplate restTemplate = new RestTemplate();		
-		ResponseEntity<EnrollmentDTO> response = 
-                       restTemplate.postForEntity(
-                           gradebook_url, 
-                           enroll, 
-                           EnrollmentDTO.class);
-		if (response.getStatusCodeValue() == 200) {
-			// update database
-			EnrollmentDTO enr = response.getBody();
-			Enrollment enrollment = enrollmentRepository.findByEmailAndCourseId(
-                                        enr.studentEmail(), enr.courseId());
-			enrollmentRepository.save(enrollment);		
-		} else {
-			// error.
-			System.out.println(
-                         "Error: unable to post gradebook"+
-                          response.getStatusCodeValue());
-		}
-
-		
-		
+		EnrollmentDTO dto = new EnrollmentDTO(0, student_email, student_name, course_id);
+		EnrollmentDTO result = restTemplate.postForObject(gradebook_url + "/enrollment/", dto, EnrollmentDTO.class);
+		System.out.println("POST result " + result);
 	}
 	
+	@Autowired
+	EnrollmentRepository enrollmentRepository;
 	/*
 	 * endpoint for final course grades
 	 */
@@ -82,12 +48,14 @@ public class GradebookServiceREST implements GradebookService {
 	@Transactional
 	public void updateCourseGrades( @RequestBody FinalGradeDTO[] grades, @PathVariable("course_id") int course_id) {
 		System.out.println("Grades received "+grades.length);
-		
-		//TODO update grades in enrollment records with grades received from gradebook service		
-		for(int i = 0; i < grades.length; i++) {
-			Enrollment e2 = enrollmentRepository.findByEmailAndCourseId(grades[i].studentEmail(), course_id);
-			e2.setCourseGrade(grades[i].grade());
-			enrollmentRepository.save(e2);
-		}	
+		for (FinalGradeDTO dto: grades) {
+			Enrollment enrollment = enrollmentRepository.findByEmailAndCourseId(dto.studentEmail(), dto.courseId());
+			if (enrollment !=null) {
+				enrollment.setCourseGrade(dto.grade());
+				enrollmentRepository.save(enrollment);
+			} else {
+				System.out.println("Error. Received final grade but could not find enrollment. "+dto.toString());
+			}
+		}
 	}
 }
